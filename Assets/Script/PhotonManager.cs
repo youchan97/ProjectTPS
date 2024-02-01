f77f8540594c1730374cbcc6c5f2f94fa2d103a9
PangGom <88571548+PangGom@users.noreply.github.com>
2024-02-01 12:16:08 +0900

    로비제작

--- Assets/Script/PhotonManager.cs
+++ Assets/Script/PhotonManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
@@ -9,6 +10,7 @@ using Photon.Realtime;
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
     public static PhotonManager Instance = null;
+
     public TMP_InputField nickNameInput;           // 닉네임 입력
     public TMP_InputField roomCodeInput;           // 방코드 입력
     public TMP_InputField roomNameInput;           // 방이름
@@ -27,9 +29,12 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     private void Awake()
     {
         if (Instance == null)
+        {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
         else
-            Destroy(Instance);
+            Destroy(gameObject);
     }
 
     void Start()
@@ -72,9 +77,8 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         {
             Debug.Log("방 이름을 설정하세요");
         }
-
     }
-    public void RandomRoom()
+    public void RandomRoom()//랜덤입장
     {
         if (PhotonNetwork.IsConnected)
         {
@@ -84,7 +88,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         else
         {
             Debug.Log("방이 없음");
-            PhotonNetwork.ConnectUsingSettings();
         }
     }
     public void CodeRoom()
@@ -94,6 +97,15 @@ public class PhotonManager : MonoBehaviourPunCallbacks
             Debug.Log("코드룸 누름");
             PhotonNetwork.JoinRoom("방이름이 들어갈 변수");//매개변수를 받아 코드로 들어가게 하는거            
         }
+    }
+    //여기서 클릭해서 입장하려면 클릭시 방이름 저장하고 매개변수 전달
+
+    public void Disconnect() => PhotonNetwork.Disconnect();//나가기 버튼
+    public void JoinLobby()//로비참가 버튼
+    {
+        Debug.Log("로비에 참가중입니다.");
+
+        PhotonNetwork.JoinLobby();
     }
     #endregion
 
@@ -112,20 +124,18 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         Debug.Log("OnConnectedToMaster 서버 접속 완료.");
         //createRoomButton.interactable = true;
     }
-
-    public override void OnDisconnected(DisconnectCause cause)
-    {
-        Debug.Log("OnDisconnected 연결끊김.");
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("방이없어요, 혹은 들어갈 수 없어요"); //아무방이나 들어가려 했는데 없을 때 호출되는 함수
-    }
+    public override void OnDisconnected(DisconnectCause cause) => Debug.Log("OnDisconnected 연결끊김.");
+    public override void OnCreatedRoom() => Debug.Log("방만들기 완료");
+    public override void OnCreateRoomFailed(short returnCode, string message) => Debug.Log("방 만들기 실패");
+    public override void OnJoinRoomFailed(short returnCode, string message) => Debug.Log("방 참가 실패"); 
+    public override void OnJoinRandomFailed(short returnCode, string message) => Debug.Log("방 랜덤참가 실패");
+    //아무방이나 들어가려 했는데 없을 때 호출되는 함수
     public override void OnJoinedRoom()
     {
-        Debug.Log(PhotonNetwork.CurrentRoom.Name + "이름의 방에 들어옴");
-        Debug.Log(PhotonNetwork.LocalPlayer.NickName + "이 방에 들어왔음");
+        Debug.Log(PhotonNetwork.CurrentRoom.Name + "이름의 방에 들어옴" + "\r\n" +
+                  PhotonNetwork.LocalPlayer.NickName + "이 방에 들어왔음");
+        //SceneManager.LoadScene("02. Lobby");
     }
+    public override void OnJoinedLobby() => Debug.Log("로비에 연결되었습니다.");
     #endregion
 }
